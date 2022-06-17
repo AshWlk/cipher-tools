95de206fd8be1d7b683b23f33b4afa59b734ca5a
Ashley Walker <ashwlk@outlook.com>
2022-06-17 20:27:12 +0100

    Create basic input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Input from './components/input/Input';
 
 const App = () => {
+  const [value, setValue] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input label='Label' valueUpdated={(newVal) => setValue(newVal)} />
+      <br />
+      <div>
+        The value of the control is {value}
+      </div>
     </div>
   );
 }
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Input.css';
 
-const Input = () => {
+export interface InputProps {
+  label: string;
+  value?: string;
+  valueUpdated?: (newValue: string) => void;
+}
+
+const Input = (props: InputProps): JSX.Element => {
+  const [value, setValue] = useState(props.value ?? '');
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value;
+    props.valueUpdated?.(value);
+    setValue(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Input'>
+      <label>{props.label}</label>
+      <input
+        type='text'
+        onChange={handleOnChange}
+        value={value}
+      />
     </div>
   );
 }
