6652b30ae5d21615f84211c86450541f277b0e73
Ashley Walker <ashwlk@outlook.com>
2022-06-19 09:35:27 +0100

    Remove console log

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ import { shiftCharacter } from "./functions/shift-character/shift-character";
 
 const App = () => {
    const [value, setValue] = useState<Record<string, string>>();
-   const handleOnStateChange = (state: Record<string, string>): void => {
-      setValue(state);
-      console.log(state);
-   };
 
    const getEncodedCharacter = (): string => {
       return value?.char && isDefined(+value?.offset) ? shiftCharacter(value.char, +value.offset) : "";
@@ -18,7 +14,7 @@ const App = () => {
 
    return (
       <div style={{ maxWidth: "300px" }}>
-         <Form onStateChange={handleOnStateChange}>
+         <Form onStateChange={setValue}>
             <Input label="Character" name="char" maxLength={1} pattern="^[a-zA-Z]$" />
             <Input label="Offset Value" name="offset" pattern="^-?[0-9]*$" />
             <Input label="Encoded Character" name="encoded-char" readOnly disabled value={getEncodedCharacter()} />
